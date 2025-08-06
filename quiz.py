be96b7b413f9fb88d9bc155ea6052fe042df947f
dilshan3110 <nagrad3134@masseyhigh.school.nz>
2025-08-07 09:40:20 +1200

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -1,6 +1,5 @@
 import random
 
-
 def string_checker(question, valid_ans=("yes", "no")):
 
     error = f"Please enter a valid option from the following list: {valid_ans}"
@@ -24,10 +23,8 @@ def string_checker(question, valid_ans=("yes", "no")):
         print(error)
         print()
 
-
 def instructions():
     print('''
-
     **** Instructions ****
 
     To begin, choose the number of questions you would like to do
@@ -42,22 +39,42 @@ def instructions():
     Good luck on the quiz.
         ''')
 
+def int_check(question, low=None, high=None, exit_code=None):
+    """Checks integers if it is above low or below high and exit code."""
+    # if any integer is allowed...
+    if low is None and high is None:
+        error = "Please enter an integer"
 
-def int_check(question):
+    # if the number needs to be more than an
+    # integer (ie: rounds / 'high number')
+    elif low is not None and high is None:
+        error = (f"Please enter an integer that is "
+                 f"more than / equal to {low}")
+
+    # if the number needs to between low & high
+    else:
+        error = (f"Please enter an integer that"
+                 f" is between {low} and {high} (inclusive)")
+
     while True:
-        error = "Please enter an integer that is 1 or more"
-        to_check = input(question)
+        response = input(question).lower()
 
-        # check for infinite mode
-        if to_check == "":
-            return "infinite"
+        # check for infinite mode / exit code
+        if response == exit_code:
+            return response
 
         try:
-            response = int(to_check)
+            response = int(response)
 
-            # checks that the number is more than / equal to 1
-            if response < 1:
+            # check the integer is not too low...
+            if low is not None and response < low:
                 print(error)
+
+            # check response is more than the low number
+            elif high is not None and response > high:
+                print(error)
+
+            # if response is valid, return it
             else:
                 return response
 
@@ -65,50 +82,13 @@ def int_check(question):
             print(error)
 
 
-
-def get_operations():
-    print("Choose operations you want to be tested on in this quiz")
-    print("Press + for Addition Questions, - For Subtraction Questions, * For Multiplication Questions, / For Division Questions")
-    print("Press <enter> for all operations")
-    # operations that can be used/valid
-    valid_ops = ["+", "-", "*", "/"]
-    while True:
-        response = input("Your choice: ").replace(" ", "")
-        if response == "":
-            return valid_ops
-        selected = response.split(",")
-        if all(op in valid_ops for op in selected):
-            return selected
-        else:
-            print("invalid choice, try enter a valid operation from the following list, +,-,*,/or just press <enter>")
-
-def generate_question(ops):
-    operation = random.choice(operation_list)
-    a = random.randint(1, 10)
-    b = random.randint(1, 10)
-    if operation == '-':
-        if a < b:
-            a, b = b, a
-        answer = a - b
-    if operation == '+':
-        answer = a + b
-    if operation == '/':
-        answer = a
-        a = a * b
-    if operation == '*':
-        answer = a * b
-    question = (f"{a} {operation} {b}")
-    return question, answer
-
-
+# stored variables
+valid_ops = ["+", "-", "*", "/"]
+game_history = []
 rounds_played = 0
 correct_count = 0
 incorrect_count = 0
-questions_asked = 0
-mode = "Regular"
-
-operation_list = ['+', '-', '/', '*']
-game_history = []
+mode = "regular"
 
 # Main Routine
 print("Welcome to Math Quiz\n")
@@ -117,11 +97,11 @@ want_instructions = string_checker("Do you want to see instructions? ")
 if want_instructions == "yes":
     instructions()
 
-num_rounds = int_check("How many rounds would you like? Push <enter> for infinite mode: ")
+num_rounds = int_check("How many rounds would you like? Push <enter> for infinite mode: ", low=1, exit_code="")
 
-if num_rounds == "infinite":
+if num_rounds == "":
     mode = "infinite"
-    num_rounds = 999999999999999999
+    num_rounds = 1
 
 # Game loop starts here
 while rounds_played < num_rounds:
@@ -132,32 +112,60 @@ while rounds_played < num_rounds:
 
     print(rounds_heading)
     print()
-    question, correct_answer = generate_question(operation_list)
-    print(f"What is {question}? (Type 'xxx' if you want to quit")
-    user_input =  int_check("your answer:")
 
-    if user_input == "xxx":
+    # printing operations into questions
+    operation = random.choice(valid_ops)
+    a = random.randint(1, 10)
+    b = random.randint(1, 10)
+    answer = ""
+    if operation == '-': # if a problem is subtraction, make the first number bigger than the seconds it doesn't become a negative.
+        if a < b:
+            a, b = b, a
+        answer = a - b
+    if operation == '+':
+        answer = a + b
+    if operation == '/': # if question/problem is division, make the first number amount to A times B and then answer = a
+        answer = a
+        a = a * b
+    if operation == '*':
+        answer = a * b
+
+    user_answer = int_check(f"{a} {operation} {b} = ", low=0, exit_code="xxx")
+
+    if user_answer == "xxx":
         break
 
+    if user_answer == answer:
+        feedback = "You are CORRECT"
+        correct_count += 1
+    else:
+        feedback = "You are WRONG"
 
+    # print infinite mode
+    if mode == "infinite":
+        print(feedback, f"Answer was {answer}")
+        num_rounds += 1
 
-    # todo ask for user ans
-
-    # todo compare with ans, if correct/wrong, store history
-
-
+    # print question answer after problem is solved and shows if answer is correct or incorrect.
+    quiz_history = f"Question {rounds_played + 1}: {feedback}. Answer is {answer}"
+    game_history.append(quiz_history)
 
     rounds_played += 1
 
+# print quiz summary
+if rounds_played > 0:
+    print("\nResults")
+    print()
     if mode == "infinite":
-        num_rounds += 1
+        num_rounds -= 1
+    print(f"You got {correct_count}/{num_rounds} correct 🎊")
+    print()
 
+    if mode == "regular":
+        see_results = string_checker("Do you want to see quiz results / answers?")
+        if see_results == "yes":
+            for something in game_history:
+                print(something)
 
-
-# todo end loop, print something, also result eg: 5/10
-
-# todo ask for hist
-if string_checker("Do you want to see Quiz summery?") == "yes":
-    print("Your Quiz summery")
-
-
+else: # print message if you exit code without answering questions
+    print("\nYou did not answer any questions and quit")
